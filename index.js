a25e26635dab824f69b9e4be00ce6e2479e18b4c
DiegoMD4 <diegolink4@hotmail.com>
2023-05-23 12:36:41 -0600

    file charge working

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
+
+app.use('/images',express.static(__dirname +'./public/uploads'));
+app.set('json spaces',2);
+app.use(express.json());
+app.use(cors());
 
 app.set('port',process.env.PORT || 4000);
 
 app.use("/", require('./src/routes/Musica'))
+app.use("/", require('./src/routes/imagenes'))
+
+
 
 app.listen(app.get('port'),(req,res)=>{
     console.log(`Server started at http://localhost:${app.get('port')}`)
---
+++ src/routes/Imagenes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) =>{
+        cb(null, "./public/uploads");
+    },
+    filename:(req,file,cb)=>{
+        const ext = file.originalname.split(".").slice(-1)[0];
+        cb(null,`${file.filename}-${Date.now()}.${ext}`);
+    }
+})
+
+const upload = multer({storage});
+
+router.post('/upload',upload.single('file'),(req, res) =>{
+    let uriFile = `http://${req.hostname}:${req.socket.localPort}/images/${req.file.filename}`
+    res.send({
+        url: uriFile
+    });
+});
+
+module.exports = router;
